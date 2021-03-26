7b2d95e5925afb74ebef76fe414d9ace87984228
eberuy <80983929+eberuy@users.noreply.github.com>
2021-03-26 15:56:45 -0300

    Add files via upload

--- version3/serializers.py
+++ version3/serializers.py
@@ -1,9 +1,16 @@
 # serializers.py
 from rest_framework import serializers
 
-from .models import Chofer
+from .models import Chofer, Contrato
 
-class ChoferSerializer (serializers.HyperlinkedModelSerializer): 
+class ChoferSerializer(serializers.HyperlinkedModelSerializer): 
     class Meta: 
         model = Chofer
         fields = ('id_chofer', 'nombre_chofer', 'email_chofer', 'pass_chofer')
+
+class ContratoSerializer(serializers.HyperlinkedModelSerializer):
+	# choferes = serializers.PrimaryKeyRelatedField(many = True, read_only = True)
+	class Meta: 
+		model = Contrato
+		fields = ('id_contrato', 'id_usuario', 'direccion', 'barrio', 'destino', 'cant_asientos', 'estado', 'comentarios', 'fecha', 'hora')
+		
