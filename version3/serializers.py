45c1e91b6343f6a9850447d44c156f9c9d3ccecf
eberuy <80983929+eberuy@users.noreply.github.com>
2021-04-14 00:26:04 -0300

    Update serializers.py

--- version3/serializers.py
+++ version3/serializers.py
@@ -1,7 +1,7 @@
 # serializers.py
 from rest_framework import serializers
 
-from .models import Chofer, Contrato, User
+from .models import Chofer, Contrato
 
 class ChoferSerializer(serializers.HyperlinkedModelSerializer): 
     class Meta: 
